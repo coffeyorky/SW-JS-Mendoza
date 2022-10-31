012c5082d554b13c032fa4445b31de8931f6c943
hunter <rossitamb@hotmail.com>
2022-10-31 02:27:55 -0300

    js

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 let nombre = prompt("Ingresa tu nombre");
-alert("Buenas, "+nombre+"!");
-console.log ("El nombre del usuario es:"+nombre+"");
+alert("Buenas, " + nombre + "!");
+console.log("El nombre del usuario es:" + nombre + "");
 
 
-const dondeComera =prompt("Desea que se le entregue el pedido en un establlecimiento?")
-let bolsa
-if (dondeComera.toLowerCase() == "si"){
-    alert("Debera seleccionar donde desea buscarlo")
-}else if(dondeComera.toLowerCase() == "no"){
-    alert("Puede continuar entonces")
-}else{
-    alert("Puede proceder a pagar")
+let llevar = parseInt(prompt("Desea que se le entregue el pedido en un establecimiento? 1.Si - 2.No"))
+
+let dondeComera = false
+let opcion
+
+while (dondeComera === false){
+    if (llevar === 1 ) {
+    dondeComera = true;
+    opcion = "Debera seleccionar donde desea buscarlo"
+  } else if (llevar === 2 ) {
+    dondeComera = true;
+    opcion = "Puede continuar"
+  } else {
+    llevar = parseInt(prompt("Debe elegir una opcion 1.Si - 2.No"))
+  }
+}
+  console.log(opcion)
+
+function calcularPrecio(precio, descuento) {
+  let resultado = precio / descuento;
+  return resultado;
 }
 
+let precio = parseInt(prompt("Ingresa el precio de la compra"));
+let descuento = parseInt(prompt("Ingrese el descuento"));
 
-function calcularPrecio(precio,descuento){
-    let resultado = precio / descuento;
-    return resultado
-}
-
-let precio = parseInt(prompt("Ingresa el precio de la compra"))
-let descuento = parseInt(prompt("Ingrese el descuento"))
-
-let resultadoDiv = calcularPrecio(precio,descuento)
-console.log(resultadoDiv)
-alert("El total es: $" +(resultadoDiv));
+let resultadoDiv = calcularPrecio(precio, descuento);
+console.log(resultadoDiv);
+alert("El total es: $" + resultadoDiv);
