f702ae0cbd2bc6eab87b9140761e3f0f780a460e
hunter <rossitamb@hotmail.com>
2022-12-12 14:18:14 -0300

    js-final

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const botonIngresar = document.getElementById("ingresar")
 const divSaludo = document.getElementById("saludo")
 const divLogin = document.getElementById("divLogin")
 
+// inicio de sesion
 botonIngresar.onclick = () => {
   const usuario = {
     usuario: inputUsuario.value
@@ -16,21 +17,23 @@ botonIngresar.onclick = () => {
   divSaludo.append(titSaludo)
 }
 
-
+// inicio de sesion
+// cuerpo
 class Swmerch {
-  constructor(id, nombre, precio) {
+  constructor(id, nombre, precio, img) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
+    this.img = img;
   }
 }
 
-const producto0 = new Swmerch(0, "Funko", 200);
-const producto1 = new Swmerch(1, "Comics", 70);
-const producto2 = new Swmerch(2, "Figura Black Series", 90);
-const producto3 = new Swmerch(3, "Coleccionables", 100);
-const producto4 = new Swmerch(4, "Remera Bad batch", 100);
-const producto5 = new Swmerch(5, "Tasa Star wars", 120);
+const producto0 = new Swmerch(0, "Funko", 200, "../img/funko.jpg");
+const producto1 = new Swmerch(1, "Comics", 70, "../img/comic.jpeg");
+const producto2 = new Swmerch(2, "Figura Black Series", 90, "../img/rex.jpg");
+const producto3 = new Swmerch(3, "Coleccionables", 100, "../img/tmand.jpg");
+const producto4 = new Swmerch(4, "Remera Bad batch", 100, "../img/thbbr.jpg");
+const producto5 = new Swmerch(5, "Tasa Star wars", 120, "../img/taza.png");
 
 const productos = [
   producto0,
@@ -45,6 +48,7 @@ const divProductos = document.getElementById("divProductos")
 productos.forEach(producto=>{
   divProductos.innerHTML +=
   "<div id=" + [producto.id]+ " class=\"card cardProd\">" +
+  "<img src=" + [producto.img] + ">" +
   "<div class= card-body>" +
   "<h4 class= card-title>" + [producto.nombre] + "</h4>"+
   "<p class= card-text>" + [producto.precio] + "</p>" + 
@@ -76,6 +80,7 @@ botonesAgregar.forEach(boton=>{
   }
 }
 )
+const cotonAlert = document.getElementById("sweetAlerta")
 
 const botonFinalizar = document.querySelector("#finalizar")
  botonFinalizar.onclick = () => {
@@ -91,4 +96,31 @@ console.log(valores)
 const botonLimpiar = document.querySelector("#limpiar")
 botonLimpiar.onclick = () => {
   localStorage.clear(carrito)
+  Swal.fire({
+  title: "Carrito vaciado, actualice la pagina.",
+  icon: "success"
+})
 }
+
+const preDolar = document.getElementById("preDolar")
+
+const cargarDolar = () => {
+  fetch("https://criptoya.com/api/dolar")
+  .then(response => response.json())
+  .then(({solidario, ccl, mep, ccb, blue}) => {
+  console.log(solidario, ccl, mep, ccb, blue)
+  preDolar.innerHTML +=
+  "<h2>Cotizaciones del dolar en caso de necesitar conversion</h2>" +
+  "<p>Solidario: $" + [solidario] + "</p>" +
+  "<p>Blue: $" + [blue] + "</p>" +
+  "<p>CCL: $" + [ccl] + "</p>" +
+  "<p>Mep: $" + [mep] + "</p>" +
+  "<p>CCB: $" + [ccb] + "</p>" 
+})
+}
+cargarDolar()
+setInterval (() => { 
+  cargarDolar()
+}, 60000);
+
+// cuerpo
